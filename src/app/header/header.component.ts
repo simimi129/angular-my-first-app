57954714ee672e5c958f71a4b3ea237faaa2bb3d
simimi129 <simimi129@gmail.com>
2023-01-12 15:37:40 +0100

    adding firebase

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { DataService } from '../shared/data.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private dataService: DataService) {}
+
+  onSaveData() {
+    this.dataService.storeRecipes();
+  }
+}
--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,5 +1,20 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class DataService {
-  constructor(private http: HttpClientModule) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    return this.http
+      .put(
+        'https://ng-recipe-4bdd7-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
