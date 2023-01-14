fe3b2cb9214dffe5923b9589edcd0989ac7ddf1b
simimi129 <simimi129@gmail.com>
2023-01-14 13:11:52 +0100

    adding auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -39,5 +40,12 @@ export class authService {
           return throwError(errorMessage);
         })
       );
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBppDlC9E4-cPqmTsvC1MjEHLoA5pOa1pw',
+      { email: email, password: password, returnSecureToken: true }
+    );
   }
 }
