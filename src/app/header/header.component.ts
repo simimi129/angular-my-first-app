a813a588a5bb30c4e0ee5f9f2f31f2ebd93bdb71
simimi129 <simimi129@gmail.com>
2023-01-14 13:36:19 +0100

    adding auth

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { DataService } from '../shared/data.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  constructor(private dataService: DataService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated: boolean = false;
+  private userSub: Subscription;
+
+  constructor(
+    private dataService: DataService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !!user;
+    });
+  }
 
   onSaveData() {
     this.dataService.storeRecipes();
@@ -15,5 +29,9 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dataService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 }
