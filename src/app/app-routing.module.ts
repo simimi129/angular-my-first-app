f658a3ed0dfbd18ac6bd6b4cb7c7dc989d1767ef
simimi129 <simimi129@gmail.com>
2023-01-19 14:28:32 +0100

    adding lazy-loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,30 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-
-import { AuthComponent } from "./auth/auth.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: "", redirectTo: "/recipes", pathMatch: "full" },
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   {
-    path: "recipes",
+    path: 'recipes',
     loadChildren: () =>
-      import("./recipes/recipes.module").then(m => m.RecipesModule)
+      import('./recipes/recipes.module').then((m) => m.RecipesModule),
   },
   {
-    path: "shopping-list",
+    path: 'shopping-list',
     loadChildren: () =>
-      import("./shopping-list/shopping-list.module").then(
-        m => m.ShoppingListModule
-      )
+      import('./shopping-list/shopping-list.module').then(
+        (m) => m.ShoppingListModule
+      ),
   },
   {
-    path: "auth",
-    loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule)
-  }
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
