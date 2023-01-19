171f02a84d1536ed6bed3626be29cc2c5bd5508a
simimi129 <simimi129@gmail.com>
2023-01-19 13:55:10 +0100

    adding modules

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -11,27 +11,6 @@ import { AuthGuard } from './auth/auth.guard';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  {
-    path: 'recipes',
-    component: RecipesComponent,
-    canActivate: [AuthGuard],
-    children: [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent },
-      {
-        path: ':id',
-        component: RecipeDetailComponent,
-        resolve: [ResolverService],
-      },
-      {
-        path: ':id/edit',
-        component: RecipeEditComponent,
-        resolve: [ResolverService],
-      },
-    ],
-  },
-  { path: 'shopping-list', component: ShoppingListComponent },
-  { path: 'auth', component: AuthComponent },
 ];
 
 @NgModule({
