037a0ad28d5a0825efd319d123f26c0831f094dc
simimi129 <simimi129@gmail.com>
2023-01-22 16:28:09 +0100

    adding ngrx

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -21,7 +21,6 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  user = new BehaviorSubject<User>(null);
   private tokenExpirationTimer: any;
 
   constructor(
